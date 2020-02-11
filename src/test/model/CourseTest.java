af616d5479de158376657a31378b8b5f6bc2e287
mertbarutcuoglu <kmbarutcuoglu19@my.sevkoleji.k12.tr>
2020-02-11 15:19:54 -0800

    Fixed the tests for the Course class.

--- src/test/model/CourseTest.java
+++ src/test/model/CourseTest.java
@@ -8,13 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CourseTest {
     Course testCourse;
 
-    @BeforeEach
-    public void runBefore(){
-        testCourse = new Course("PHIL", "220", "005");
-    }
-
     @Test
     public void testCourse(){
+        testCourse = new Course("PHIL", "220", "005");
         double courseAverage = (79.65 + 77.87 + 79.96) / 3.0;
         assertEquals(testCourse.getCourseID(), "PHIL");
         assertEquals(testCourse.getCourseNumber(), "220");
